565d9be1c43462c14aa470189d231e8ef178c56b
Lahu S. Andhale <111334418+Lahu19@users.noreply.github.com>
2023-07-22 21:52:19 +0530

    routers are added by Lahu

--- collabv/src/App.jsx
+++ collabv/src/App.jsx
@@ -6,11 +6,21 @@ import Home from './components/home/Home';
 import Learning from './components/learning_modules/Learning';
 import Navbar from './components/attributes/Navbar';
 import Footer from './components/attributes/Footer';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <>
-    
+    <Router>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/learning" element={<Learning />} />
+      </Routes>
+      <Footer/>
+    </Router>
     </>
   );
 }
