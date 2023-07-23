8f54f02d5488180b8ec4a57c38686cbd5836f949
Lahu S. Andhale <111334418+Lahu19@users.noreply.github.com>
2023-07-23 20:30:58 +0530

    crousel initial setup by Lahu

---
+++ collabv/src/components/home/slider/main_slider.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './main_slider.css'
+import SimpleImageSlider from "react-simple-image-slider";
+const images = [
+    { url: "https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg?auto=compress&cs=tinysrgb&w=600" },
+    {url:"https://images.pexels.com/photos/5926393/pexels-photo-5926393.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+    { url: "https://images.pexels.com/photos/1293120/pexels-photo-1293120.jpeg?auto=compress&cs=tinysrgb&w=600" },
+    { url: "https://images.pexels.com/photos/247819/pexels-photo-247819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+    { url: "https://images.pexels.com/photos/7562114/pexels-photo-7562114.jpeg?auto=compress&cs=tinysrgb&w=600" },
+
+];
+
+export class main_slider extends Component {
+    render() {
+        return (
+            <div className="slider-container">
+                <SimpleImageSlider className="image-slider"
+                    width={1520}
+                    height={580}
+                    images={images}
+                    showBullets={true}
+                    showNavs={true}
+                    autoPlay={true}
+                    autoPlayDelay={true}
+                    
+                />
+            </div>
+        )
+    }
+}
+
+export default main_slider
