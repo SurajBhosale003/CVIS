9b0fc3a897d5b1ab17ac746a64a474425f255fac
SnehalMarutiSalokhe <snehal.maruti.salokhe@gmail.com>
2023-10-29 11:20:53 +0530

    added reset password functionality

--- collabv/src/components/attributes/Register.jsx
+++ collabv/src/components/attributes/Register.jsx
@@ -1,83 +1,114 @@
-// // import React, { Component } from 'react'
 
-// // export class Register extends Component {
-// //     signup = () => {
+// import React, { Component } from 'react';
+// import "./logincss.css"
 
-// //     }
-// //     render() {
-// //         return (
-// //             <div>
-// //                 <div id="login-form-wrap">
-// //                     <h2>Registration</h2>
-// //                     <form id="login-form">
-// //                         <p>
-// //                             <input type="text" id="username" placeholder="Enter your full name" required />
-// //                             <i className="validation">
-// //                                 <span></span>
-// //                                 <span></span>
-// //                             </i>
-// //                         </p>
-// //                         <p>
-// //                             <input type="email" id="email" placeholder="Email Address" required />
-// //                             <i className="validation">
-// //                                 <span></span>
-// //                                 <span></span>
-// //                             </i>
-// //                         </p>
+// import { createUserWithEmailAndPassword } from 'firebase/auth'; 
+// import { auth } from '../../Firebase'; 
+
+// class Register extends Component {
+//   signup = () => {
+//     const email = document.getElementById('email').value;
+//     const password = document.getElementById('password').value;
+
+//     const authInstance = auth; 
+
+//     createUserWithEmailAndPassword(authInstance, email, password)
+//       .then((userCredential) => {
+//         const user = userCredential.user;
+//         console.log('User registered:', user);
+//         alert('Registration successful! You can now sign in.');
+//         window.location.href = 'http://localhost:5173/login'; 
+//       })
+//       .catch((error) => {
+//         if (error.code === 'auth/email-already-in-use') {
+//           alert('A user with this email address already exists. Please sign in instead.');
+//           window.location.href = 'http://localhost:5173/login';
+//         } else if (error.code === 'auth/weak-password') {
+//           alert('Invalid password. Please enter a stronger password.');
+//           document.getElementById('password').value = ''; 
+//         } else {
+//           console.error('Registration error:', error);
+//         }
+//       });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <div id="login-form-wrap">
+//           <h2>Registration</h2>
+//           <form
+//             id="login-form"
+//             onSubmit={(e) => {
+//               e.preventDefault();
+//               this.signup();
+//             }}
+//           >
+//             <p>
+//               <input type="text" id="username" placeholder="Enter your full name" required />
+//               <i className="validation">
+//                 <span></span>
+//                 <span></span>
+//               </i>
+//             </p>
+//             <p>
+//               <input type="email" id="email" placeholder="Email Address" required />
+//               <i className="validation">
+//                 <span></span>
+//                 <span></span>
+//               </i>
+//             </p>
+//             <p>
+//               <input type="password" id="password" placeholder="Enter password" required />
+//               <i className="validation">
+//                 <span></span>
+//                 <span></span>
+//               </i>
+//             </p>
+//             <p>
+//               <input type="submit" id="register" value="Register" />
+//             </p>
+//           </form>
+//           <div id="create-account-wrap">
+//             <p>Already a member? <a href="http://localhost:5173/login">Sign in</a></p>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Register;
 
 
-// //                         <p>
-// //                             <input type="password" id="password" placeholder="Enter password" required />
-// //                             <i className="validation">
-// //                                 <span></span>
-// //                                 <span></span>
-// //                             </i>
-// //                         </p>
-// //                         <p>
-// //                             <input type="submit" id="register" value="Register" />
-// //                         </p>
-// //                     </form>
-// //                     <div id="create-account-wrap">
-// //                         <p>A member? <a href="http://localhost:5173/login">Sign in</a></p>
-// //                         {/* Change the link before deployement */}
-// //                     </div>
-// //                 </div>
-
-// //             </div>
-// //         )
-// //     }
-// // }
-
-// // export default Register
 
 
-import React, { Component } from 'react';
-import "./logincss.css"
-
-import { createUserWithEmailAndPassword } from 'firebase/auth'; 
-import { auth } from '../../Firebase'; 
+import  { Component } from 'react';
+import './logincss.css';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../Firebase';
 
 class Register extends Component {
   signup = () => {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    const authInstance = auth; 
-
-    createUserWithEmailAndPassword(authInstance, email, password)
+    createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
         console.log('User registered:', user);
         alert('Registration successful! You can now sign in.');
-        window.location.href = 'http://localhost:5173/login'; 
+        window.location.href = 'http://localhost:5173/login';  {/* Change the link before deployment */}
       })
       .catch((error) => {
+        // Handle registration errors
         if (error.code === 'auth/email-already-in-use') {
           alert('A user with this email address already exists. Please sign in instead.');
           window.location.href = 'http://localhost:5173/login';
+            {/* Change the link before deployment */}
         } else if (error.code === 'auth/weak-password') {
           alert('Invalid password. Please enter a stronger password.');
-          document.getElementById('password').value = ''; 
+          document.getElementById('password').value = '';
         } else {
           console.error('Registration error:', error);
         }
@@ -86,6 +117,7 @@ class Register extends Component {
 
   render() {
     return (
+     
       <div>
         <div id="login-form-wrap">
           <h2>Registration</h2>
@@ -123,11 +155,14 @@ class Register extends Component {
           </form>
           <div id="create-account-wrap">
             <p>Already a member? <a href="http://localhost:5173/login">Sign in</a></p>
+              {/* Change the link before deployment */}
           </div>
         </div>
       </div>
+   
     );
   }
 }
 
 export default Register;
+
--- collabv/src/components/attributes/login.jsx
+++ collabv/src/components/attributes/login.jsx
@@ -1,55 +1,75 @@
-// import React, { Component } from 'react'
 
 
-// export class login extends Component {
-    
-//     signin=()=>{
+// import { Component } from 'react';
+// import "./logincss.css"
+// import { signInWithEmailAndPassword } from 'firebase/auth'; 
+// import { auth } from '../../Firebase'; 
 
-//     }
-//     render() {
-//         return (
-//             <div>
-//                 <div id="login-form-wrap">
-//                     <h2>Login</h2>
-//                     <form id="login-form">
-//                     <p>
-//                             <input type="email" id="email"  placeholder="Email Address" required />
-//                             <i className="validation">
-//                                 <span></span>
-//                                 <span></span>
-//                             </i>
-//                         </p>
-                
-                       
-//                         <p>
-//                             <input type="password" id="password"  placeholder="Enter password" required />
-//                             <i className="validation">
-//                                 <span></span>
-//                                 <span></span>
-//                             </i>
-//                         </p>
-//                         <p>
-//                             <input type="submit" id="login" value="Login" />
-//                         </p>
-//                     </form>
-//                     <div id="create-account-wrap">
-//                         <p>Not a member? <a href="http://localhost:5173/auth">Create Account</a></p>
-//                         {/* Change the link before deployement */}
-//                     </div>
-//                 </div>
+// class Login extends Component {
+//   signIn = () => {
+//     const email = document.getElementById('email').value;
+//     const password = document.getElementById('password').value;
 
-//             </div>
-//         )
-//     }
+//     signInWithEmailAndPassword(auth, email, password)
+//       .then((userCredential) => {
+//         // User signed in successfully
+//         const user = userCredential.user;
+//         console.log('User signed in:', user);
+//       })
+//       .catch((error) => {
+//         // Handle any sign-in errors here
+//         console.error('Sign-in error:', error);
+//       });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <div id="login-form-wrap">
+//           <h2>Login</h2>
+//           <form
+//             id="login-form"
+//             onSubmit={(e) => {
+//               e.preventDefault();
+//               this.signIn();
+//             }}
+//           >
+//             <p>
+//               <input type="email" id="email" placeholder="Email Address" required />
+//               <i className="validation">
+//                 <span></span>
+//                 <span></span>
+//               </i>
+//             </p>
+//             <p>
+//               <input type="password" id="password" placeholder="Enter password" required />
+//               <i className="validation">
+//                 <span></span>
+//                 <span></span>
+//               </i>
+//             </p>
+//             <p>
+//               <input type="submit" id="login" value="Login" />
+//             </p>
+//           </form>
+//           <div id="create-account-wrap">
+//             <p>Not a member? <a href="http://localhost:5173/auth/register">Create Account</a></p>
+//             {/* Change the link before deployment */}
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
 // }
 
-// export default login
+// export default Login;
 
 
-import React, { Component } from 'react';
-import "./logincss.css"
-import { signInWithEmailAndPassword } from 'firebase/auth'; // Import the necessary Firebase Auth function
-import { auth } from '../../Firebase'; // Import the Firebase auth instance
+
+import  { Component } from 'react';
+import './logincss.css';
+import { signInWithEmailAndPassword, sendPasswordResetEmail } from 'firebase/auth';
+import { auth } from '../../Firebase';
 
 class Login extends Component {
   signIn = () => {
@@ -58,18 +78,33 @@ class Login extends Component {
 
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        // User signed in successfully
         const user = userCredential.user;
         console.log('User signed in:', user);
+        
+        window.location.href = 'http://localhost:5173'; // Replace with your desired URL
       })
       .catch((error) => {
-        // Handle any sign-in errors here
+        // Handle sign-in errors
         console.error('Sign-in error:', error);
+        alert('Invalid email or password. Please try again.');
+      });
+  };
+
+  resetPassword = () => {
+    const email = document.getElementById('email').value;
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        alert('Password reset email sent. Please check your inbox.');
+      })
+      .catch((error) => {
+        console.error('Password reset error:', error);
+        alert('Unable to send a password reset email. Please try again.');
       });
   };
 
   render() {
     return (
+     
       <div>
         <div id="login-form-wrap">
           <h2>Login</h2>
@@ -101,12 +136,14 @@ class Login extends Component {
           <div id="create-account-wrap">
             <p>Not a member? <a href="http://localhost:5173/auth/register">Create Account</a></p>
             {/* Change the link before deployment */}
+            <p> Forgot Password? <a onClick={this.resetPassword}> Reset Password</a></p>
+
           </div>
         </div>
       </div>
+    
     );
   }
 }
 
 export default Login;
-
