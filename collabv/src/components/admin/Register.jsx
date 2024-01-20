ca1d85b5715d06c35112613e9b80837c87ad66a5
SnehalMarutiSalokhe <snehal.maruti.salokhe@gmail.com>
2024-01-20 19:07:04 +0530

    css updated

--- collabv/src/components/admin/Register.jsx
+++ collabv/src/components/admin/Register.jsx
@@ -1,21 +1,23 @@
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { UserAuth } from '../../context/AuthContext';
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../../context/AuthContext";
+
+import "./logincss.css";
 
 const Register = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   // eslint-disable-next-line no-unused-vars
-  const [error, setError] = useState('')
+  const [error, setError] = useState("");
   const { createUser } = UserAuth();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError('');
+    setError("");
     try {
       await createUser(email, password);
-      navigate('/account')
+      navigate("/account");
     } catch (e) {
       setError(e.message);
       console.log(e.message);
@@ -24,44 +26,36 @@ const Register = () => {
   };
 
   return (
-    <div >
-      <div>
-        <h1 >Sign up for a free account</h1>
-        <p className='py-2'>
-          Already have an account yet?{' '}
-          <Link to='/signin' className='underline'>
+    <div>
+      <div className="login">
+        <h1>Sign up for a free account</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Email Address</label>
+            <input onChange={(e) => setEmail(e.target.value)} type="email" />
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+            />
+          </div>
+          <button>Sign Up</button>
+        </form>
+      </div>
+
+      <div className="login">
+        <p className="py-2">
+          Already have an account yet?{" "}
+          <Link to="/signin" >
             Sign in.
           </Link>
         </p>
-        <p >
-          If admin then login here{' '}
-          <Link to='/adminLogin' >
-           admin Sign in.
-          </Link>
-
+        <p>
+          If admin then login here <Link to="/adminLogin">admin Sign in.</Link>
         </p>
       </div>
-      <form onSubmit={handleSubmit}>
-        <div >
-          <label >Email Address</label>
-          <input
-            onChange={(e) => setEmail(e.target.value)}
-           
-            type='email'
-          />
-        </div>
-        <div>
-          <label >Password</label>
-          <input
-            onChange={(e) => setPassword(e.target.value)}
-            
-            type='password'
-          />
-        </div>
-        <button >
-          Sign Up
-        </button>
-      </form>
     </div>
   );
 };
