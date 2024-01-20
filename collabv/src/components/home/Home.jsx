058baf27c0a42582d4db0f1040f91947ae3b8bc7
SnehalMarutiSalokhe <snehal.maruti.salokhe@gmail.com>
2024-01-20 12:52:17 +0530

    added cvis is ISO certified company.

--- collabv/src/components/home/Home.jsx
+++ collabv/src/components/home/Home.jsx
@@ -1,18 +1,44 @@
-import './Home.css'
-import Cards from './allCards/Cards'
-import BookDemo from './bookDemo/BookDemo'
-import Main_slider from './slider/main_slider'
-import Ourfeedback from './ourfeed/ourfeedback'
+
+import  { useState, useEffect } from 'react';
+import './Home.css';
+import Cards from './allCards/Cards';
+import BookDemo from './bookDemo/BookDemo';
+import MainSlider from './slider/main_slider';
+import OurFeedback from './ourfeed/ourfeedback';
+
 function Home() {
+  const [isHighlighted, setHighlighted] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const threshold = 400;
+
+      if (scrollY > threshold) {
+        setHighlighted(true);
+      } else {
+        setHighlighted(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
-    
-    <Main_slider/>
-    <Cards/>
-    <BookDemo/>
-    <Ourfeedback/>
+      <MainSlider />
+      <div className={`highlight-container ${isHighlighted ? 'highlighted' : ''}`}>
+        <h3>Collab Vision InfoSolution is ISO certified company.</h3>
+      </div>
+      <Cards />
+      <BookDemo />
+      <OurFeedback />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
