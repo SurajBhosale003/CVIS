fcc4e211a8bde63feb5dfbf5fe3b96c94c5f5470
Lahu S. Andhale <111334418+Lahu19@users.noreply.github.com>
2024-01-21 14:29:58 +0530

    i have added the login css

--- collabv/src/components/admin/Register.jsx
+++ collabv/src/components/admin/Register.jsx
@@ -1,21 +1,23 @@
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { UserAuth } from '../../context/AuthContext';
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../../context/AuthContext";
+
+import "./logincss.css";
 
 const Register = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   // eslint-disable-next-line no-unused-vars
-  const [error, setError] = useState('')
+  const [error, setError] = useState("");
   const { createUser } = UserAuth();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError('');
+    setError("");
     try {
       await createUser(email, password);
-      navigate('/account')
+      navigate("/account");
     } catch (e) {
       setError(e.message);
       console.log(e.message);
@@ -24,46 +26,118 @@ const Register = () => {
   };
 
   return (
-    <div >
-      <div>
-        <h1 >Sign up for a free account</h1>
-        <p className='py-2'>
-          Already have an account yet?{' '}
-          <Link to='/signin' className='underline'>
+    <div>
+      <div className="login">
+        <h1>Sign up for a free account</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Email Address</label>
+            <input onChange={(e) => setEmail(e.target.value)} type="email" />
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+            />
+          </div>
+          <button>Sign Up</button>
+        </form>
+      </div>
+
+      <div className="login">
+        <p className="py-2">
+          Already have an account yet?{" "}
+          <Link to="/signin" >
             Sign in.
           </Link>
         </p>
-        <p >
-          If admin then login here{' '}
-          <Link to='/adminLogin' >
-           admin Sign in.
-          </Link>
-
+        <p>
+          If admin then login here <Link to="/adminLogin">admin Sign in.</Link>
         </p>
       </div>
-      <form onSubmit={handleSubmit}>
-        <div >
-          <label >Email Address</label>
-          <input
-            onChange={(e) => setEmail(e.target.value)}
-           
-            type='email'
-          />
-        </div>
-        <div>
-          <label >Password</label>
-          <input
-            onChange={(e) => setPassword(e.target.value)}
-            
-            type='password'
-          />
-        </div>
-        <button >
-          Sign Up
-        </button>
-      </form>
     </div>
   );
 };
 
 export default Register;
+// import { useState } from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+// import { UserAuth } from '../../context/AuthContext';
+// import './loginc.css'
+// const Register = () => {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+//   // eslint-disable-next-line no-unused-vars
+//   const [error, setError] = useState('')
+//   const { createUser } = UserAuth();
+//   const navigate = useNavigate()
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     setError('');
+//     try {
+//       await createUser(email, password);
+//       navigate('/account')
+//     } catch (e) {
+//       setError(e.message);
+//       console.log(e.message);
+//       alert(e.message);
+//     }
+//   };
+
+//   return (
+//     <div className='bodylogin'>
+//       <div id="login-form-wrap">
+//         <h2 >Sign up for a free account</h2>
+
+//         <form onSubmit={handleSubmit} id="login-form">
+//           <p>
+//             <input
+//               onChange={(e) => setEmail(e.target.value)}
+//               type='email'
+//               className='input1'
+//               placeholder='Enter Email address'
+//               required
+//             />
+//             <i className="validation">
+//               <span></span>
+//               <span></span>
+//             </i>
+//           </p>
+//           <p>
+//           <input
+//             onChange={(e) => setPassword(e.target.value)}
+//             className='input1'
+//             placeholder='Enter password'
+//             type='password'
+//             required
+//           />
+//               <i className="validation">
+//                 <span></span>
+//                 <span></span>
+//               </i>
+//             </p>
+//           <p>
+//               <input className='input1' type="submit" id="login" value="Login" />
+//             </p>
+//         </form>
+//         <p >
+//           Already have an account yet?{' '}
+//           <Link to='/signin' className='underline'>
+//             Sign in.
+//           </Link>
+//         </p>
+//         <p >
+//           If admin then login here{' '}
+//           <Link to='/adminLogin' >
+//             admin Sign in.
+//           </Link>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
+
+
+// export default Register;
